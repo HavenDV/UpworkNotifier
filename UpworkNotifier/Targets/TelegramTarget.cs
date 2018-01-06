b9149117486c1690dedc42f2115ea75e17de0e35
HavenDV <havendv@gmail.com>
2018-01-06 15:02:40 +1000

    Implemented TelegramTarget

--- UpworkNotifier/Targets/TelegramTarget.cs
+++ UpworkNotifier/Targets/TelegramTarget.cs
@@ -1,9 +1,31 @@
-﻿namespace UpworkNotifier.Targets
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace UpworkNotifier.Targets
 {
     public class TelegramTarget : ITarget
     {
-        public void SendMessage(string text)
+        #region Properties
+
+        public TelegramBotClient Client { get; }
+        public ChatId ChatId { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public TelegramTarget(string token, int userId)
         {
+            Client = new TelegramBotClient(token);
+            ChatId = new ChatId(userId);
         }
+
+        #endregion
+
+        #region ITarget
+
+        public async void SendMessage(string text) => await Client.SendTextMessageAsync(ChatId, text);
+
+        #endregion
     }
 }
