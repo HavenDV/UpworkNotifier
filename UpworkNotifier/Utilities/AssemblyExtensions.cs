0e7b1ca0e90af2f221988786ea98cbdac087531b
HavenDV <havendv@gmail.com>
2018-02-16 20:54:53 +1000

    Added AssemblyExtensions.GetDllPaths method

--- UpworkNotifier/Utilities/AssemblyExtensions.cs
+++ UpworkNotifier/Utilities/AssemblyExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -11,5 +13,18 @@ namespace UpworkNotifier.Utilities
 
         public static T[] GetObjectsOfInterface<T>(this Assembly assembly) =>
             assembly.GetTypesOfInterface<T>().Select(Activator.CreateInstance).Cast<T>().ToArray();
+
+        public static string[] GetDllPaths(this Assembly assembly)
+        {
+            var list = new List<string> { assembly.Location };
+
+            var folder = Path.GetDirectoryName(assembly.Location) ?? "";
+            list.AddRange(assembly.GetReferencedAssemblies()
+                .Select(i => i.Name)
+                .Where(i => !i.StartsWith("System.") && !i.StartsWith("mscorlib"))
+                .Select(i => Path.Combine(folder, i + ".dll")));
+
+            return list.ToArray();
+        }
     }
 }
