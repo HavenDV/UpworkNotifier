9e4c82fda6c51e7d6823641721e17308cfe53500
HavenDV <havendv@gmail.com>
2018-02-17 02:16:20 +1000

    Changed TelegramTarget to module

--- H.NET.Targets.TelegramTarget/TelegramTarget.cs
+++ H.NET.Targets.TelegramTarget/TelegramTarget.cs
@@ -4,22 +4,56 @@ using Telegram.Bot.Types;
 
 namespace H.NET.Targets
 {
-    public class TelegramTarget : ITarget
+    public class TelegramTarget : Module, ITarget
     {
         #region Properties
 
-        private TelegramBotClient Client { get; }
-        private ChatId ChatId { get; }
+        private int _userId;
+        public int UserId
+        {
+            get => _userId;
+            set {
+                _userId = value;
+
+                if (!IsValid())
+                {
+                    return;
+                }
+
+                ChatId = new ChatId(value);
+            }
+        }
+
+        private string _token;
+        public string Token
+        {
+            get => _token;
+            set {
+                _token = value;
+
+                if (!IsValid())
+                {
+                    return;
+                }
+
+                Client = new TelegramBotClient(value);
+            }
+        }
+
+        private TelegramBotClient Client { get; set; }
+        private ChatId ChatId { get; set; }
 
         #endregion
 
         #region Constructors
 
-        public TelegramTarget(string token, int userId)
+        public TelegramTarget()
         {
-            Client = new TelegramBotClient(token);
-            ChatId = new ChatId(userId);
+            AddSetting("Token", o => Token = (string)o, o => o is string, string.Empty);
+            AddSetting("UserId", o => UserId = (int)o, o => o is int, 0);
         }
+
+        public override bool IsValid() => base.IsValid() && !string.IsNullOrWhiteSpace(Token) && UserId > 0;
 
         #endregion
 
