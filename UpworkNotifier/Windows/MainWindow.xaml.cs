55f6a655ef6687c0ec479cf8ac24572e13e5e67b
HavenDV <havendv@gmail.com>
2018-02-17 21:43:27 +1000

    Deleted all settings and resourses

--- UpworkNotifier/Windows/MainWindow.xaml.cs
+++ UpworkNotifier/Windows/MainWindow.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Windows;
 using H.NET.Core;
-using H.NET.Notifiers;
-using H.NET.Targets;
-using UpworkNotifier.Properties;
 using UpworkNotifier.Utilities;
 
 namespace UpworkNotifier.Windows
 {
-    public partial class MainWindow : IDisposable
+    public partial class MainWindow
     {
-        #region Properties
-
-        private INotifier Notifier { get; set; }
-        private ITarget Target { get; set; }
-
-        #endregion
-
         #region Constructors
 
         public MainWindow()
@@ -26,62 +15,11 @@ namespace UpworkNotifier.Windows
 
             Visibility = Visibility.Hidden;
 
-            LoadSettings();
+            Global.GenerateGetTargetByNameFuncFromModules(() => ModuleManager.ActiveModules);
 
-            Settings.Default.SettingChanging += (o, args) => SaveButton.IsEnabled = true;
-        }
-
-        private void LoadSettings()
-        {
-            // Dispose object if exists
-            Dispose();
-
-            var errors = false;
-
-            // Load Screenshot Notifier
-            var path = Settings.Default.ScreenshotExamplePath;
-            var interval = Settings.Default.ScreenshotInterval;
-            if (File.Exists(path) && interval > 0)
-            {
-                Notifier = new UpworkScreenshotNotifier{ ExamplePath = path, Interval = interval };
-                Notifier.AfterEvent += OnNotifierOnAfterScreenshot;
-            }
-            else
-            {
-                Log(Properties.Resources.Failed_to_load_Screenshot_Notifier__Path_or_Interval_is_invalid);
-                errors = true;
-            }
-
-            // Load Telegram Target
-            var token = Settings.Default.TelegramToken.Trim();
-            var userId = Settings.Default.TelegramUserId;
-            if (!string.IsNullOrWhiteSpace(token) && userId > 0)
-            {
-                Target = new TelegramTarget{ Token = token, UserId = userId };
-            }
-            else
-            {
-                Log(Properties.Resources.Failed_to_load_Telegram_Target_Token_or_UserId_is_invalid);
-                errors = true;
-            }
-
-            if (errors)
-            {
-                return;
-            }
-
-            // Show message
-            Log(Properties.Resources.Settings_successfully_loaded);
-        }
-
-        #endregion
-
-        #region IDisposable
-
-        public void Dispose()
-        {
-            Notifier?.Dispose();
-            Notifier = null;
+            Log("Loading modules...");
+            var unused = ModuleManager.ActiveModules;
+            Log("Loaded");
         }
 
         #endregion
@@ -102,6 +40,7 @@ namespace UpworkNotifier.Windows
 
         private void Close_Click(object sender, RoutedEventArgs e) => Close();
 
+        /*
         private void ScreenshotExamplePathButton_Click(object sender, RoutedEventArgs e)
         {
             var path = DialogUtilities.OpenFileDialog(ScreenshotExamplePathTextBox.Text);
@@ -110,27 +49,8 @@ namespace UpworkNotifier.Windows
                 return;
             }
 
-            Settings.Default.ScreenshotExamplePath = path;
-            Settings.Default.Save();
         }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            Settings.Default.Save();
-            SaveButton.IsEnabled = false;
-
-            LoadSettings();
-        }
-
-        private void OnNotifierOnAfterScreenshot(object sender, EventArgs args)
-        {
-            Log("Notifier send event AfterScreenshot");
-
-            var message = Settings.Default.Message;
-            Target?.SendMessage(string.IsNullOrWhiteSpace(message)
-                ? Properties.Resources.Message_is_empty__Please_set_the_message_in_the_General_Settings
-                : message);
-        }
+        */
 
         private void Settings_Click(object sender, RoutedEventArgs e) => new SettingsWindow().ShowDialog();
 
