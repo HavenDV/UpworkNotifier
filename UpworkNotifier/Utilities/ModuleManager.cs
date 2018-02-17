b3a1e6cd42e767fbccd3709107d485d3b413e6b0
HavenDV <havendv@gmail.com>
2018-02-17 12:47:21 +1000

    Added ModuleManager class

---
+++ UpworkNotifier/Utilities/ModuleManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace UpworkNotifier.Utilities
+{
+    public static class ModuleManager
+    {
+        #region Properties
+
+        public static string AppDataFolder { get; } = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public static string BaseFolder { get; } = CombineAndCreate(AppDataFolder, "H.NET");
+        public static string GetModuleFolder(Assembly assembly) => CombineAndCreate(BaseFolder, assembly.GetName().Name);
+
+        #endregion
+
+        #region Public methods
+
+        public static Assembly[] Load() => Directory
+            .EnumerateDirectories(BaseFolder)
+            .Select(folder => Path.Combine(folder, $"{Path.GetFileName(folder) ?? ""}.dll"))
+            .Where(i => !string.IsNullOrWhiteSpace(i))
+            .Where(File.Exists)
+            .Select(Assembly.LoadFile)
+            .ToArray();
+
+        public static void Install(Assembly assembly)
+        {
+            var toFolder = GetModuleFolder(assembly);
+            var files = assembly.GetDllPaths();
+
+            foreach (var path in files)
+            {
+                var name = Path.GetFileName(path) ?? throw new Exception($"Path is not valid");
+                File.Copy(path, Path.Combine(toFolder, name));
+            }
+        }
+
+        public static void Deinstall(Assembly assembly)
+        {
+            var toFolder = GetModuleFolder(assembly);
+            Directory.Delete(toFolder, true);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string CombineAndCreate(params string[] arguments) =>
+            Directory.CreateDirectory(Path.Combine(arguments)).FullName;
+
+        #endregion
+    }
+}
