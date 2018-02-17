50083c9aa409ea11d6cc6be3f6b2dc94032b4452
HavenDV <havendv@gmail.com>
2018-02-17 21:43:59 +1000

    Added UpworkScreenshowNotifier Target and Message properties

--- H.NET.Notifiers.UpworkScreenshotNotifier/UpworkScreenshotNotifier.cs
+++ H.NET.Notifiers.UpworkScreenshotNotifier/UpworkScreenshotNotifier.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using System.IO;
 using Emgu.CV;
+using H.NET.Core;
 using H.NET.Notifiers.Extensions;
 using H.NET.Notifiers.Utilities;
 
@@ -29,6 +30,9 @@ namespace H.NET.Notifiers
 
         private Mat Mask { get; set; }
 
+        public string Target { get; set; }
+        public string Message { get; set; }
+
         #endregion
 
         #region Contructors
@@ -36,6 +40,10 @@ namespace H.NET.Notifiers
         public UpworkScreenshotNotifier()
         {
             AddSetting("ExamplePath", o => ExamplePath = o, PathIsValid, string.Empty);
+            AddSetting("Target", o => Target = o, TargetIsValid, string.Empty);
+            AddSetting("Message", o => Message = o, StringIsValid, string.Empty);
+
+            AfterEvent += (sender, args) => Global.GetTargetByName(Target)?.SendMessage(Message);
         }
 
         #endregion
@@ -43,6 +51,10 @@ namespace H.NET.Notifiers
         #region Protected methods
 
         public bool PathIsValid(string path) => !string.IsNullOrWhiteSpace(path) && File.Exists(path);
+
+        public bool StringIsValid(string path) => !string.IsNullOrWhiteSpace(path);
+
+        public bool TargetIsValid(string target) => !string.IsNullOrWhiteSpace(target) && Global.GetTargetByName(target) != null;
 
         protected override bool Analyze(Bitmap bitmap)
         {
