54747a634e44a59c39ae939b89487bd83eda1a72
HavenDV <havendv@gmail.com>
2018-02-19 00:04:12 +1000

    Renamed UpworkScreenshotNotifier to MaskScreenshotNotifier

--- H.NET.Notifiers.MaskScreenshotNotifier/Properties/AssemblyInfo.cs
+++ H.NET.Notifiers.MaskScreenshotNotifier/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("H.NET.Notifiers.UpworkScreenshotNotifier")]
+[assembly: AssemblyTitle("H.NET.Notifiers.MaskScreenshotNotifier")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("H.NET.Notifiers.UpworkScreenshotNotifier")]
+[assembly: AssemblyProduct("H.NET.Notifiers.MaskScreenshotNotifier")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
