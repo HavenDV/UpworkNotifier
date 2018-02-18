b1f2a8527c8336cd2c489d73d7b6e61208f5b38f
HavenDV <havendv@gmail.com>
2018-02-18 18:13:56 +1000

    Added AssemblyExtensions reference dlls fix

--- UpworkNotifier/Utilities/AssemblyExtensions.cs
+++ UpworkNotifier/Utilities/AssemblyExtensions.cs
@@ -21,13 +21,19 @@ namespace UpworkNotifier.Utilities
             var list = new List<string> { assembly.Location };
 
             var folder = assembly.GetFolder();
-            list.AddRange(assembly.GetReferencedAssemblies()
-                .Select(i => i.CodeBase ?? Path.Combine(folder, i.Name + ".dll"))
+            var references = assembly.GetReferencedAssemblies()
+                .Select(i => i.CodeBase?.Replace(@"file:///", string.Empty) ?? Path.Combine(folder, i.Name + ".dll"))
                 .Where(i => !Path.GetFileName(i).StartsWith("System.") &&
-                            !Path.GetFileName(i).StartsWith("mscorlib")));
+                            !Path.GetFileName(i).StartsWith("mscorlib"))
+                .ToArray();
+            list.AddRange(references);
+            list.AddRange(references.Select(Assembly.LoadFrom).SelectMany(GetDllPaths));
 
+            // Emgu.CV and other wrapper fix
             list.AddRange(GetFilesIfExists(Path.Combine(folder, "x86")));
             list.AddRange(GetFilesIfExists(Path.Combine(folder, "x64")));
+
+            list = list.Distinct(StringComparer.OrdinalIgnoreCase).ToList();
 
             return list.ToArray();
         }
