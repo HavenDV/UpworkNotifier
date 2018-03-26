4ac8b5b0f531a9670747eecd29fd4b62dace58d4
HavenDV <havendv@gmail.com>
2018-03-26 18:40:11 +1000

    Added Converters to Core

---
+++ H.NET.Core/Converters/Converter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace H.NET.Core.Converters
+{
+    public class Converter : Module
+    {
+        #region Properties
+
+        public Exception Exception { get; protected set; }
+
+        #endregion
+    }
+}
